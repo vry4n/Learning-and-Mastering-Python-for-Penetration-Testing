4d3baabcc39c3495f582350c7bf6f1e5984fbc2f
Vry4n <46062162+vry4n@users.noreply.github.com>
2023-12-14 19:01:44 -0600

    Create web-directory-enum.py

---
+++ web-directory-enum.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import requests
+import os
+
+# Replace 'http://example.com' with the target website URL
+base_url = "https://vk9-sec.com"
+
+# Replace 'wordlist.txt' with the path to your wordlist file
+wordlist_path = "../../wordlist.txt"
+
+def enumerate_files(base_url, wordlist):
+    for word in wordlist:
+        # contruct the path
+        url = f"{base_url}/{word}" + "/"
+        # request using HEAD method looking for return code
+        response = requests.head(url)
+        # if the code 
+        if response.status_code == 200:
+            print(f"File found: {url}")
+
+# Check if the wordlist file exists
+if os.path.exists(wordlist_path):
+    # read the file
+    with open(wordlist_path, "r") as file:
+        # iterate through each line
+        wordlist = [line.strip() for line in file.readlines()]
+
+    # Call the function to enumerate files
+    enumerate_files(base_url, wordlist)
+else:
+    # check if the wordlist location is right
+    print(f"Wordlist file not found: {wordlist_path}")
